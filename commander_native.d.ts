c2873c3f410e4c67c545af92654bbbf7cbf0bed1
Uwe Riegel <uwriegel@gmail.com>
2017-10-16 21:11:51 +0200

    t.d.ts extended

--- commander_native.d.ts
+++ commander_native.d.ts
@@ -9,4 +9,5 @@ interface FileItem {
 declare module 'commander_native' {
     function getFiles(path: string, callback: (error: any, result: FileItem[]) => void): void
     function getIcon(extension: string, callback: (error: any, result: Buffer) => void): void
+    function getExtendedInfos(path: string, files: string[], callback: (err, res: string[]) => void): void
 }
